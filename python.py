2d02e70bffe1f0e0f636e6cd2ffbb4c0061ace45
Anddy <92260109+AnndyCep@users.noreply.github.com>
2024-11-28 19:36:15 -0500

    subiendo archivo python

---
+++ python.py
@@ -0,0 +1,15 @@
+def suma(*num):
+    return sum(num);
+    
+print(suma(1,2,4,10,25,5));
+"""Comentario sin gestion"""
+"""Dejando nuevo comentario
+porque el codigo ya esta refactorizado"""
+
+print("Mostrar primerto este texto")
+
+
+print("Hola haciendo prueba Uno ramas")
+print("Hola haciendo prueba Dos ramas")
+
+print("Probando git reflog")
