46c971b2a7c1a874e3637c889c767ead418e7fe3
Soy Andres <hannsonandrey@hotmail.com>
2024-12-01 22:02:03 -0500

    Dejando funcion suma, como eliminanfo comentarios de python

--- python.py
+++ python.py
@@ -2,15 +2,4 @@ def suma(*num):
     return sum(num);
     
 print(suma(1,2,4,10,25,5));
-"""Comentario sin gestion"""
-"""Dejando nuevo comentario
-porque el codigo ya esta refactorizado"""
-
-print("Mostrar primerto este texto")
-
-
-print("Hola haciendo prueba Uno ramas")
-print("Hola haciendo prueba Dos ramas")
-
-print("Probando git reflog")
-print("cambiando nombre")
+"""Se eliminaron unos comentarios y unas lineas que se imprimian"""
--- python2.py
+++ python2.py
@@ -1,1 +1,6 @@
 print("Esto es una pruena de git pull")
+
+def funcionSuma(*num):
+    """Esto retorna una suna de todos los numeros pasados en la funncion"""
+    return sum(num)
+
